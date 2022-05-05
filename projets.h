3873ce37706f1c0b86e7c40b8e7806d90e4eb707
Yanis Labbe <ylabbe@c1r1p2.42quebec.com>
2022-05-05 16:32:20 -0400

    Header fix

--- projets.h
+++ projets.h
@@ -1,5 +1,17 @@
-#ifndef MAKEFILE_H
-# define MAKEFILE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   projets.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylabbe <ylabbe@student.42quebec.c>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 16:30:56 by ylabbe            #+#    #+#             */
+/*   Updated: 2022/05/05 16:31:58 by ylabbe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROJETS_H
+# define PROJETS_H
 
 # include "./get_next_line/get_next_line.h"
 # include "./libft/libft.h"
